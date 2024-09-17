a11c2a38ea985683dd9b1382ed553abec5b8983e
kitkat242 <wwwmaximkornienko@gmail.com>
2024-09-17 19:16:51 +0300

    Добавьте файлы проекта.

---
+++ Factory Method Lib/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace Factory_Method_Lib
+{
+    namespace FactoryMethodLib
+    {
+        public interface ITestButton
+        {
+            Control CreateButton();
+        }
+
+        public class StandardButtonFactory : ITestButton
+        {
+            public Control CreateButton()
+            {
+                Button button = new Button();
+                button.Text = "Standard Button";
+                button.Size = new System.Drawing.Size(150, 50);
+                return button;
+            }
+        }
+
+        // Конкретна фабрика для RadioButton
+        public class RadioButtonFactory : ITestButton
+        {
+            public Control CreateButton()
+            {
+                RadioButton radioButton = new RadioButton();
+                radioButton.Text = "Radio Button";
+                radioButton.Size = new System.Drawing.Size(150, 50);
+                return radioButton;
+            }
+        }
+
+        public class CheckBoxButtonFactory : ITestButton
+        {
+            public Control CreateButton()
+            {
+                CheckBox checkBox = new CheckBox();
+                checkBox.Text = "Check Box";
+                checkBox.Size = new System.Drawing.Size(150, 50);
+                return checkBox;
+            }
+        }
+    }
+
+}
---
+++ Factory method/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Factory_Method_Lib.FactoryMethodLib;
+
+namespace Factory_method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ITestButton standardButtonFactory = new StandardButtonFactory();
+            AddButton(standardButtonFactory, new Point(50, 50));
+
+            ITestButton radioButtonFactory = new RadioButtonFactory();
+            AddButton(radioButtonFactory, new Point(50, 120));
+
+            ITestButton checkBoxFactory = new CheckBoxButtonFactory();
+            AddButton(checkBoxFactory, new Point(50, 190));
+        }
+
+        private void AddButton(ITestButton buttonFactory, Point location)
+        {
+            Control control = buttonFactory.CreateButton();
+            control.Location = location;
+            this.Controls.Add(control);
+        }
+    }
+}
+
+
